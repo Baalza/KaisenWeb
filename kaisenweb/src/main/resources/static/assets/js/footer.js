d747545ab53442844bcef2b3eafd293bd08725b0
Baalza <niccolobalzarotti@gmail.com>
2023-03-13 22:22:22 +0100

    Fix
    General Fix

--- kaisenweb/src/main/resources/static/assets/js/footer.js
+++ kaisenweb/src/main/resources/static/assets/js/footer.js
@@ -1,51 +1,23 @@
 const footer = document.createElement("template");
 
 footer.innerHTML = /*html*/ `
-<footer class="fixed-bottom ">
-      <div class="container-fluid b">
-        <div class="container">
-          <div class="row justify-content-center text-center pe-3">
-            <div class="col text-center align-items-center">
-              <p class="matter mt-3">Designed & Developed by</p>
-              <a class="no-dec"
-                href="https://baalza.github.io/Balzarotti-Niccolo/"
-                target="_blank"
-                ><p class="sign mb-4">Balzarotti Niccolò</p></a
-              >
-            </div>
-          </div>
-        </div>
+<footer>
+<div class="container-fluid">
+  <div class="container">
+    <div class="row justify-content-center text-center pe-3">
+      <div class="col text-center align-items-center">
+        <p class="matter mt-3">Designed & Developed by</p>
+        <a
+          class="no-dec"
+          href="https://baalza.github.io/Balzarotti-Niccolo/"
+          target="_blank"
+          ><p class="sign mb-4">Balzarotti Niccolò</p></a
+        >
       </div>
-    </footer>
+    </div>
+  </div>
+</div>
+</footer>
 `;
 
 document.body.appendChild(footer.content);
-
-class Footer extends HTMLElement {
-  constructor() {
-    super();
-  }
-
-  connectedCallback() {
-    this.innerHTML = /*html*/ `
-      <footer class="fixed-bottom ">
-      <div class="container-fluid b">
-        <div class="container">
-          <div class="row justify-content-center text-center pe-3">
-            <div class="col text-center align-items-center">
-              <p class="matter mt-3">Designed & Developed by</p>
-              <a class="no-dec"
-                href="https://baalza.github.io/Balzarotti-Niccolo/"
-                target="_blank"
-                ><p class="sign mb-4">Balzarotti Niccolò</p></a
-              >
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-      `;
-  }
-}
-
-customElements.define("footer-component", Footer);
