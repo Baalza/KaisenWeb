fd225d4a2339de8dd8fbbc6c00d981324caa3fc9
Baalza <niccolobalzarotti@gmail.com>
2023-03-13 13:54:47 +0100

    Creation
    generate spring app

---
+++ kaisenweb/src/main/java/kaisenweb/kaisenweb/KaisenwebApplication.java
@@ -0,0 +1,13 @@
+package kaisenweb.kaisenweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaisenwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaisenwebApplication.class, args);
+	}
+
+}
