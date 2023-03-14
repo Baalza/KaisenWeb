a0dfa0c8809d1d2b471cd5e9410c6b871e9ff94b
Alaa <alaaeddine.ghanimi@brainman.it>
2023-03-14 17:43:24 +0100

    created the structure of the project

--- kaisenweb/src/main/java/kaisenweb/kaisenweb/KaisenwebApplication.java
+++ kaisenweb/src/main/java/kaisenweb/kaisenweb/KaisenwebApplication.java
@@ -1,7 +1,11 @@
 package kaisenweb.kaisenweb;
 
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class KaisenwebApplication {
