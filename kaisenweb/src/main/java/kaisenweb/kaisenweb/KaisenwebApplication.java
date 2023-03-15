4b63d9804df2dcd7cc4dbccdc2a36e98b10975eb
Baalza <niccolobalzarotti@gmail.com>
2023-03-15 10:05:52 +0100

    Test API

--- kaisenweb/src/main/java/kaisenweb/kaisenweb/KaisenwebApplication.java
+++ kaisenweb/src/main/java/kaisenweb/kaisenweb/KaisenwebApplication.java
@@ -1,8 +1,15 @@
 package kaisenweb.kaisenweb;
 
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
@@ -10,8 +17,21 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class KaisenwebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KaisenwebApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(KaisenwebApplication.class, args);
+		RestTemplate restTemplate = new RestTemplate();
+        
+        String grid = restTemplate.getForObject("https://api.themoviedb.org/3/movie/popular?api_key=dfcc7abe68d35aa410d4654be1b250b4&language=it-It&page=1", String.class);
+        //System.out.println(grid.trim());
+		
+        JsonObject data = new Gson().fromJson(grid.trim(), JsonObject.class);
+		JsonArray temp = data .get("results").getAsJsonArray();
+		ArrayList<String> test = new ArrayList<String>();
+		for(JsonElement element : temp){
+            JsonObject object = element.getAsJsonObject();
+            String main = object.getAsJsonObject().get("id").getAsString();
+            test.add(main);
+        }
+		test.stream().forEach(System.out::println);
 	}
 
 }
