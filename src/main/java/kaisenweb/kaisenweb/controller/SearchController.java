acd52f2114a85c9cf594853530a8f952119fd279
Baalza <niccolobalzarotti@gmail.com>
2023-03-22 21:33:42 +0100

    Search Controller

--- src/main/java/kaisenweb/kaisenweb/controller/SearchController.java
+++ src/main/java/kaisenweb/kaisenweb/controller/SearchController.java
@@ -1,5 +1,17 @@
 package kaisenweb.kaisenweb.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
 public class SearchController {
     
+    @GetMapping("/search")
+    @ResponseBody
+    public  ModelAndView search(@RequestParam String query) {
+        ModelAndView mav = new ModelAndView("search.html");
+        System.out.println(query);
+        return mav;
+        }
 }
