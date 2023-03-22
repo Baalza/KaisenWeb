a0a0c06fcfcd70ebb6196811da23d20f4e52069d
Baalza <niccolobalzarotti@gmail.com>
2023-03-22 22:34:46 +0100

    search results

--- src/main/java/kaisenweb/kaisenweb/controller/SearchingController.java
+++ src/main/java/kaisenweb/kaisenweb/controller/SearchingController.java
@@ -1,17 +1,18 @@
 package kaisenweb.kaisenweb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-public class SearchController {
+@Controller
+public class SearchingController {
     
     @GetMapping("/search")
-    @ResponseBody
-    public  ModelAndView search(@RequestParam String query) {
+    //@ResponseBody
+    public  ModelAndView search() {//@RequestParam("query") String query
         ModelAndView mav = new ModelAndView("search.html");
-        System.out.println(query);
+        //System.out.println(query);
         return mav;
         }
 }
