b478b706657b00f5d6b0016b17ae68302dd74909
Baalza <niccolobalzarotti@gmail.com>
2023-04-03 18:00:08 +0200

    -

--- src/main/resources/static/assets/js/loadSearch.js
+++ src/main/resources/static/assets/js/loadSearch.js
@@ -11,7 +11,7 @@ const searchBar = document.createElement("template");
 searchBar.innerHTML = /*html*/ `
 <form
                 class="d-flex animate__animated animate__flipInX"
-                action="https://kaisenweb.herokuapp.com/search/movie"
+                action="http://localhost:8080/search/movie"
                 method="GET"
                 _lpchecked="1"
               >
@@ -35,7 +35,7 @@ const form = document.createElement("form");
 form.classList.add("d-flex");
 form.classList.add("animate__animated");
 form.classList.add("animate__flipInX");
-form.setAttribute("action", "https://kaisenweb.herokuapp.com/search/movie");
+form.setAttribute("action", "http://localhost:8080/search/movie");
 form.setAttribute("method", "GET");
 form.setAttribute("_lpchecked", "1");
 
@@ -62,9 +62,7 @@ form.appendChild(input);
 form.appendChild(button);
 
 async function getHtml() {
-  const response = await fetch(
-    "https://kaisenweb.herokuapp.com/NumRes?query=" + query
-  );
+  const response = await fetch("http://localhost:8080/NumRes?query=" + query);
   const myJson = await response.json(); //extract JSON from the http response
   return myJson;
 }
@@ -86,9 +84,7 @@ getHtml().then((data) => {
 });
 
 async function getHtml2() {
-  const response = await fetch(
-    "https://kaisenweb.herokuapp.com/NumPage?query=" + query
-  );
+  const response = await fetch("http://localhost:8080/NumPage?query=" + query);
   const myJson = await response.json(); //extract JSON from the http response
   return myJson;
 }
@@ -676,7 +672,7 @@ function loadColl() {
 }
 async function getHtml3(p, page) {
   const response = await fetch(
-    "https://kaisenweb.herokuapp.com/LoadEntity?query=" +
+    "http://localhost:8080/LoadEntity?query=" +
       query +
       "&category=" +
       p +
